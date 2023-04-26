3a22a015535e1359a3ce0eca71ab09b5341d63e8
Bruno <82094246+Freeza0@users.noreply.github.com>
2023-04-26 13:47:22 +0530

    Add files via upload

---
+++ tictactoe.js
@@ -0,0 +1,11 @@
+//document.write("hi");
+// best coding language for machine learning
+/*
+const name = "bruno";
+for (let i=0; i<4; i++) {
+	console.log(name[i]);
+}	*/
+function warn() {
+	console.log("kemcho!")
+}
+document.getElementById('box').innerHTML = 0;

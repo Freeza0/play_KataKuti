b43cfb12207c06eedd838d1a502466dedd8b83f7
Bruno <82094246+Freeza0@users.noreply.github.com>
2023-05-01 15:19:28 +0530

    Add files via upload

--- tictactoe.js
+++ tictactoe.js
@@ -5,7 +5,17 @@ const name = "bruno";
 for (let i=0; i<4; i++) {
 	console.log(name[i]);
 }	*/
-function Fun() {
-	let boxes = document.getElementByClassName('box');
-	boxes.innerHTML = "O";
+let button_state = 0; // 0, 1, 2, 3 for tree button state which can only on if the other two are off
+/*
+function Fun(idname) {
+	//if ()
+	var button = document.getElementById(idname);
+	button.style.color = 'black';
+}*/
+
+function Fun(event) {
+  const Button = event.target;
+  Button.setAttribute('style', 'color: blue');
+  Button.setAttribute('style', 'background-color: rgba(0, 0, 0, 0)');
+  Button.setAttribute('style', 'box-shadow: none');
 }
